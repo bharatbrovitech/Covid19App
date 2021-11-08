ba71fdcb25bb9613b68c99896721d60bb74b8b49
Aditya sharma <akshh05@gmail.com>
2021-11-08 23:26:41 +0530

    UI of home page top five countries

--- src/modules/home/index.js
+++ src/modules/home/index.js
@@ -1,29 +1,25 @@
 import React, { useEffect, useState ,useCallback} from 'react'
 import { StyleSheet, View, Text ,FlatList, Pressable } from 'react-native'
-import { getTopCountriesAndGlobalCasesApiCall } from '../../apis/Covid.service'
-const styles = StyleSheet.create({
-    container: {
-        flexGrow: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    text: {}
-})
+import { getTopCountriesAndGlobalCasesApiCall } from '../../apis/Covid.service';
+import { CountriesDetails } from '../../components/UI/CountriesDetails';
+
+
 const HomeScreen = ({navigation}) => {
     const [topCountries, setTopCountries] = useState([])
     const [globalData, setGlobalData] = useState(undefined)
 
-    useEffect(async()=> {
-       const globalData =  await getTopCountriesAndGlobalCasesApiCall();
-       setTopCountries(globalData.topCountriesCases);
-       setGlobalData(globalData.globalCases)
+    useEffect(()=> {
+       getTopCountriesAndGlobalCasesApiCall().then(response=>{
+        setTopCountries(response.topCountriesCases);
+        setGlobalData(response.globalCases)
+       });
     }, [])
 
-    const renderTopCovidCountries=({item})=>{
+    const renderTopCovidCountries=({item,index})=>{
         return (
-            <Pressable onPress={()=>navigation.navigate("CountryListScreen")}>
-                <Text>{item?.Country}</Text>
+            <Pressable onPress={()=>navigation.navigate("CountryListScreen")} style={[styles.listContainer,{marginLeft:index%2 == 0 ? 0:10}]}>
+                <Text style={{marginBottom:5,fontWeight:"600",color:'rgb(145,189,248)'}}>{`${item?.Country} `}</Text>
+                <Text style={{marginBottom:5,fontWeight:"500"}}>{`Total cases: ${item?.TotalConfirmed}`}</Text>
             </Pressable>
         )
     }
@@ -35,10 +31,34 @@ const HomeScreen = ({navigation}) => {
             extraData={topCountries}
             renderItem={renderTopCovidCountries}
             keyExtractor={(item)=>item.ID}
+            numColumns={2}
           />
         </View>
     )
 }
-
+const styles = StyleSheet.create({
+    container: {
+        flexGrow: 1,
+        backgroundColor: 'rgb(246,248,254)',
+        padding:20,
+    },
+    listContainer:{
+        backgroundColor:"#fff",
+        marginBottom:10,
+        paddingHorizontal:10,
+        paddingVertical:30,
+        borderRadius:15,
+            shadowColor: "rgba(0,0,0,0.5)",
+            shadowOffset: {
+                width: 0,
+                height: 2,
+            },
+            shadowOpacity: 0.25,
+            shadowRadius: 3.84,
+            elevation: 5,
+            flex:1,
+            maxWidth:'45%'
+    }
+})
 
 export default HomeScreen;
